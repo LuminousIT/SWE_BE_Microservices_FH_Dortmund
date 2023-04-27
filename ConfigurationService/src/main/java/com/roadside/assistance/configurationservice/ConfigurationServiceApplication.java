5d9a5b93bbea1f341b7522f55efd1b20fef804bf
Olumide Oluwole <oluwoleit@gmail.com>
2023-04-27 10:27:07 +0200

    base config

--- ConfigurationService/src/main/java/com/roadside/assistance/configurationservice/ConfigurationServiceApplication.java
+++ ConfigurationService/src/main/java/com/roadside/assistance/configurationservice/ConfigurationServiceApplication.java
@@ -1,15 +1,15 @@
-package de.fhdo.student.management.configurationservice;
+package com.roadside.assistance.configurationservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigurationServiceApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(ConfigurationServiceApplication.class, args);
+        SpringApplication.run(com.roadside.assistance.configurationservice.ConfigurationServiceApplication.class, args);
     }
 
 }

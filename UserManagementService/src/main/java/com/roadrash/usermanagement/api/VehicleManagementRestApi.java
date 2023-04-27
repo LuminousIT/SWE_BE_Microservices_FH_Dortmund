912aac5eedf7cb229a1f278c6adb1c11126da078
Olumide Oluwole <oluwoleit@gmail.com>
2023-04-27 15:21:19 +0200

    vehicles extrainfo

--- UserManagementService/src/main/java/com/roadrash/usermanagement/api/VehicleManagementRestApi.java
+++ UserManagementService/src/main/java/com/roadrash/usermanagement/api/VehicleManagementRestApi.java
@@ -1,11 +1,17 @@
 package com.roadrash.usermanagement.api;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.roadrash.usermanagement.domain.UserInfo;
+import com.roadrash.usermanagement.domain.VehicleDemoData;
 import com.roadrash.usermanagement.domain.VehicleInfo;
 import com.roadrash.usermanagement.service.VehicleInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,5 +43,23 @@ public class VehicleManagementRestApi {
     @PutMapping("/{id}")
     public VehicleInfo updateUser(@PathVariable Long id, @RequestBody VehicleInfo vehicleInfo) {
         return vehicleInfoService.updateVehicle(id, vehicleInfo);
+    }
+
+    @GetMapping("/information")
+    public VehicleDemoData[] getVehicleData(Model model) throws IOException {
+        File jsonFile = new ClassPathResource("vehicleInfo.json").getFile();
+        ObjectMapper objectMapper = new ObjectMapper();
+        VehicleDemoData[] testData = objectMapper.readValue(jsonFile, VehicleDemoData[].class);
+
+//        model.addAttribute("vehicleBreakdowns", testData);
+//
+//        for (VehicleDemoData vehicle : testData) {
+//            model.addAttribute("model", vehicle.getModel());
+//            model.addAttribute("timestamp", vehicle.getTimestamp().toString());
+//            model.addAttribute("location", (vehicle.getLocation().getLatitude() + "(lat) " + vehicle.getLocation().getLongitude() + "(lon)").toString());
+//            model.addAttribute("Hardware Fault Code", vehicle.getErrorCode());
+//        }
+
+        return testData;
     }
 }
---
+++ UserManagementService/src/main/java/com/roadrash/usermanagement/domain/VehicleDemoData.java
@@ -0,0 +1,62 @@
+package com.roadrash.usermanagement.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Date;
+
+public class VehicleDemoData {
+    @JsonProperty("model")
+    private String model;
+    @JsonProperty("timestamp")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
+    private Date timestamp;
+    @JsonProperty("location")
+    private GeoLocation location;
+    @JsonProperty("hardware_fault_code")
+    private String errorCode;
+
+    @JsonProperty("userID")
+    private Long userID;
+
+    public Long getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Long userID) {
+        this.userID = userID;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public GeoLocation getLocation() {
+        return location;
+    }
+
+    public void setLocation(GeoLocation location) {
+        this.location = location;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+}
